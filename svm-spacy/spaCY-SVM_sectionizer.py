e60afc69a3c1d13d8b901d384199f54c2024d25f
Paul M. Heider <heiderp@musc.edu>
2021-02-04 15:32:42 -0500

    refactored to include train/classify split; added argument flags
    I refactored main() into train and annotate (or classify) to allow
    for simple training loops vs. annotation or classification loops.
    The model is now saved via pickle to the model directory to be
    loaded later.  Annotated notes are not yet written to disk so there
    is no way to score them.  (Scoring will need to be done via ETUDE
    most likely although an initial scoring can be done internal to
    the SVM modeler but that's not a good long-term solution.)

--- svm-spacy/spaCY-SVM_sectionizer.py
+++ svm-spacy/spaCY-SVM_sectionizer.py
@@ -7,14 +7,38 @@ import spacy as spacy
 
 import pandas as pd
 
+import pickle
+
 from sklearn import svm
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
 
 from tqdm import tqdm
 
+def extract_header_index( ann_file ):
+    header_idx = []
+    with open( ann_file , 'r') as fp:
+        header_ann = fp.read().splitlines()
+        # get the index of headers
+        for ann_line in header_ann:
+            if re.search("^T[0-9]", ann_line):
+                splitted_annline = ann_line.split('\t')
+                header_idx.append(splitted_annline[1].split(" ")[-2:])
+    return( header_idx )
 
-def main(input_dir, output_dir):
+
+def check_header( index , header_idx ):
+    in_range = False
+    i = 0
+    while not in_range and i < len( header_idx ):
+        idx_range = header_idx[i]
+        if index in range(int(idx_range[0]), int(idx_range[1]) + 1): # inclusive range
+            in_range = True
+        i += 1
+    return in_range
+
+
+def extract_features( input_dir, output_dir , training_flag = False ):
     ## Load an clinical English medspaCy model trained on i2b2 data
     ## - https://github.com/medspacy/sectionizer/blob/master/notebooks/00-clinical_sectionizer.ipynb
     nlp = spacy.load('en_info_3700_i2b2_2012')
@@ -27,16 +51,11 @@ def main(input_dir, output_dir):
     filenames = [os.path.basename(f) for f in glob.glob(os.path.join(input_dir,
                                                                      '*.txt'))]  # Filenames of data to be trained on. Data must be in .txt format AND the header info must have the same filename with the extension ".ann"
     for filename in tqdm(filenames):
-        # read .ann files for creating the training data
-        filename_ann = re.sub("txt$", "ann", filename)
-        with open(os.path.join(input_dir, filename_ann), 'r') as fp:
-            header_ann = fp.read().splitlines()
-            # get the index of headers
-            header_idx = []
-            for ann_line in header_ann:
-                if re.search("^T[0-9]", ann_line):
-                    splitted_annline = ann_line.split('\t')
-                    header_idx.append(splitted_annline[1].split(" ")[-2:])
+        header_idx = None
+        if( training_flag ):
+            # read .ann files for creating the training data
+            filename_ann = re.sub("txt$", "ann", filename)
+            header_idx = extract_header_index( os.path.join( input_dir , filename_ann ) )
 
         # read .txt files to get features
         with open(os.path.join(input_dir, filename), 'r') as fp:
@@ -91,19 +110,13 @@ def main(input_dir, output_dir):
                 start_idx = end_idx - sent_len # index of the starting character
                 assert sent.text_with_ws == processed_notes.text[start_idx:end_idx] # assert the index is correct
 
-                def check_header(index):
-                    in_range = False
-                    i = 0
-                    while not in_range and i < len(header_idx):
-                        idx_range = header_idx[i]
-                        if index in range(int(idx_range[0]), int(idx_range[1]) + 1): # inclusive range
-                            in_range = True
-                        i += 1
-                    return in_range
-
-                if check_header(start_idx) & check_header(end_idx-1):
+                if( header_idx is None ):
+                    is_header, wrongly_sentencized = False , False
+                elif( check_header( start_idx , header_idx ) and
+                    check_header( end_idx-1 , header_idx ) ):
                     is_header, wrongly_sentencized = True, False
-                elif not check_header(start_idx) and not check_header(end_idx-1):
+                elif( not check_header( start_idx , header_idx ) and
+                      not check_header( end_idx-1 , header_idx ) ):
                     is_header, wrongly_sentencized = False, False
                 else: # if part of the sentence is a header, NOT consider it as a header and mark it as wrongly_sentencized
                     is_header, wrongly_sentencized = False, True
@@ -124,22 +137,46 @@ def main(input_dir, output_dir):
             # else:
             features[column] = features[column].astype(bool)
 
-    # write the pd.df as a .csv
-    features.to_csv(os.path.join(output_dir,"Processed_dataset.csv"),index=True)
-
-    # convert to binary predictors. No need to normalize the continous variable 'percentile'
-    features_clean = features.drop(['filename', 'idx1', 'idx2','content'], axis=1).dropna(axis=0)
-
     # SVM
-    target = features_clean['header']
-    predictors = features_clean.drop('header', axis=1)
-    x_train, x_test, y_train, y_test = train_test_split(predictors, target, test_size=0.3, random_state=444)
+    raw_features = features.drop( 'header' , axis=1 )
+    if( training_flag ):
+        target = features[ 'header' ]
+        return( raw_features , target )
+    else:
+        return( raw_features )
 
+
+def clean_up_features( raw_features , keep_filename = False ):
+    # convert to binary predictors. No need to normalize the continous variable 'percentile'
+    if( keep_filename ):
+        features_clean = raw_features.drop( [ 'idx1', 'idx2','content'], axis=1).dropna(axis=0)
+    else:
+        features_clean = raw_features.drop( ['filename', 'idx1', 'idx2','content'], axis=1).dropna(axis=0)
+    return( features_clean )
+
+
+def train( input_dir, output_dir , model_dir ):
+    raw_features , target = extract_features( input_dir, output_dir , training_flag = True )
+
+    # write the pd.df as a .csv
+    raw_features.to_csv( os.path.join( output_dir , "raw_processed_dataset.csv" ) ,
+                         index = True )
+    
+    predictors = clean_up_features( raw_features )
+    
+    x_train, x_test, y_train, y_test = train_test_split(predictors, target, test_size=0.3, random_state=444)
+    
     # create a classifier
+    ## TODO - make the kernal a passable argument so you can provide
+    ##        something like --svm-kernel "linear" / --svm-kernel "rbf"
+    ##        as a command line argument
     clf = svm.SVC(kernel='linear')
     # train the model using the training sets
     clf.fit(x_train, y_train)
-
+    ## Save the model to disk
+    model_file = os.path.join( model_dir , 'svm_sectionizer.pkl' )
+    pickle.dump( clf , open( model_file , 'wb' ) )
+    
     y_pred = clf.predict(x_test)
     accuracy = metrics.accuracy_score(y_test, y_pred)
     precision = metrics.precision_score(y_test, y_pred)
@@ -151,20 +188,56 @@ def main(input_dir, output_dir):
           "\nPrecision = ", precision,
           "\nRecall = ", recall)
 
-        # features: to create features, what spacy features can I use? (sentence boundary detection, pos, similarity ?, rule based matching?, text classification?)
-        # what features to use as predictors?
+    # features: to create features, what spacy features can I use? (sentence boundary detection, pos, similarity ?, rule based matching?, text classification?)
+    # what features to use as predictors?
+
+
+def annotate( input_dir, output_dir , model_file ):
+    raw_features = extract_features( input_dir, output_dir , training_flag = False )
+
+    cleaned_features = clean_up_features( raw_features , keep_filename = True )
+    cleaned_features.to_csv( os.path.join( output_dir , "cleaned_processed_dataset.csv" ) ,
+                             index = False )
+    predictors = clean_up_features( raw_features , keep_filename = False )
+    
+    ## Load the model from disk
+    clf = pickle.load( open( model_file , 'rb' ) )
+    
+    y_pred = clf.predict( predictors )
+
+    ## TODO - Iterate over the raw_features to write a CAS XMI file per filename
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Clincial notes sectionizer based on spaCY basic pipeline and SVM')
-    parser.add_argument('inputDir',
-                        help='Input directory containing plain text and .ann files to train and test the sectionizer')
-    parser.add_argument('outputDir',
+    parser.add_argument( '-i' , '--input-dir' , required = True ,
+                         dest = 'input_dir' ,
+                         help='Input directory containing plain text and .ann files to train and test the sectionizer')
+    parser.add_argument( '-o' , '--output-dir' , required = True ,
+                         dest = 'output_dir',
                         help='Output directory for writing the processed dataset to')
+    parser.add_argument( '--model-dir' , required = False ,
+                         default = None ,
+                         dest = 'model_dir',
+                         help='Output directory for writing the model details' )
+    parser.add_argument( '--train' ,
+                         dest = 'train' ,
+                         help = "Train a new model based on the input directory" ,
+                         action = "store_true" )
     args = parser.parse_args()
-    main(os.path.abspath(args.inputDir),
-         os.path.abspath(args.outputDir))
-
+    if( not os.path.exists( args.output_dir ) ):
+        os.mkdir( args.output_dir )
+    if( args.model_dir is None ):
+        args.model_dir = args.output_dir
+    if( args.train ):
+        train( os.path.abspath( args.input_dir ) ,
+               os.path.abspath( args.output_dir ) ,
+               os.path.abspath( args.model_dir ) )
+    else:
+        annotate( os.path.abspath( args.input_dir ) ,
+                  os.path.abspath( args.output_dir ) ,
+                  os.path.join( os.path.abspath( args.model_dir ) ,
+                                'svm_sectionizer.pkl' ) )
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
